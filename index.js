e281e3e480b9b03f1de181e976eccdbab2d7368a
EviSONG <evisong@live.com>
2022-07-27 15:16:16 +0800

    added sourcemap tests

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use('/hidden-sourcemap', express.static(path.join(__dirname, 'hidden-sourcemap'), {
+    setHeaders(res) {
+      const url = res.req.originalUrl;
+      if (url.endsWith('.js')) {
+        res.setHeader('SourceMap', url + '.map');
+      }
+    }
+  }))
+  .use(express.static(path.join(__dirname, 'static'),))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
