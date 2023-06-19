a2fc69c775c19f39d5a1476d3f21d196f2f61a2a
EviSONG <evisong@live.com>
2023-06-19 14:02:43 +0800

    Fix paths for Vercel

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use('/api/hidden-sourcemap', express.static(path.join(__dirname, '../', 'hidden-sourcemap'), {
+  .use('/api/hidden-sourcemap', express.static(path.join(__dirname, '../static/hidden-sourcemap'), {
     setHeaders(res) {
       const url = res.req.originalUrl;
       if (url.endsWith('.js')) {
